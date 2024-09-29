f5dfcb6f66c4a966c912d4e5e4af6a97e09ad5f5
Sajid <yeamimhossainsajid@gmail.com>
2024-09-30 02:15:41 +0600

    first commit

---
+++ src/test/java/com/uiudevelopershub/thinktanku/ThinkTankUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uiudevelopershub.thinktanku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkTankUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
