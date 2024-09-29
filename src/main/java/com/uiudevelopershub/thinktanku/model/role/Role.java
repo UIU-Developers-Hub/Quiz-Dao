0c7422554d4094869d7725ba2ce13ae11f4b6efd
Sajid <yeamimhossainsajid@gmail.com>
2024-09-30 02:54:48 +0600

    Added User and Role entity

---
+++ src/main/java/com/uiudevelopershub/thinktanku/model/role/Role.java
@@ -0,0 +1,40 @@
+package com.uiudevelopershub.thinktanku.model.role;
+
+import com.uiudevelopershub.thinktanku.model.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+//@SuperBuilder
+//@SQLRestriction( "is_active = TRUE" )
+//@SQLDelete( sql = "UPDATE role SET is_active = FALSE WHERE id = ?" )
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column( nullable = false )
+    private String roleType = "USER";
+
+    @ManyToMany(
+            mappedBy = "roles",
+            cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH }
+    )
+    private Set<User> users = new LinkedHashSet<>();
+
+    public Role(String roleType ) {
+        this.roleType = roleType;
+    }
+}
---
+++ src/main/java/com/uiudevelopershub/thinktanku/model/user/User.java
@@ -0,0 +1,51 @@
+package com.uiudevelopershub.thinktanku.model.user;
+
+import com.uiudevelopershub.thinktanku.model.role.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+//@SuperBuilder
+//@SQLRestriction( "is_active = TRUE" )
+//@SQLDelete( sql = "UPDATE _user SET is_active = FALSE WHERE id = ?" )
+@Entity
+@Table( name = "_user" )
+////@NamedEntityGraph(
+//        name = "User.roles",
+//        attributeNodes = @NamedAttributeNode("roles")
+//)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column( nullable = false )
+    private String username;
+
+    @Column( nullable = false )
+    private String email;
+
+    @Column( nullable = false )
+    private String password;
+
+    @ManyToMany(
+            cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH },
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "_user_roles",
+            joinColumns = @JoinColumn( name = "user_id" ),
+            inverseJoinColumns = @JoinColumn( name = "roles_id" )
+    )
+    private Set<Role> roles = new LinkedHashSet<>();
+}
