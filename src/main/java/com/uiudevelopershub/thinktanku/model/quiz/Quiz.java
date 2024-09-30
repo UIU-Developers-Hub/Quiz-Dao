0e8baa9e165f317c9b29d5c919d54a7c9b2169b7
Sajid <yeamimhossainsajid@gmail.com>
2024-09-30 17:53:02 +0600

    Added Quiz and QuizSession Entity

---
+++ src/main/java/com/uiudevelopershub/thinktanku/model/quiz/Quiz.java
@@ -0,0 +1,64 @@
+package com.uiudevelopershub.thinktanku.model.quiz;
+
+import com.uiudevelopershub.thinktanku.model.quizsession.QuizSession;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Quiz {
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "quiz_gen" )
+    @SequenceGenerator( name = "quiz_gen", sequenceName = "quiz_seq", allocationSize = 1 )
+    @Column( name = "id", nullable = false )
+    private Long id;
+
+    @Column( nullable = false )
+    private String questionTitle;
+
+    @Column( nullable = false )
+    private String questionAnswer;
+
+    @Column( nullable = false )
+    private String optionOne;
+
+    @Column( nullable = false )
+    private String optionTwo;
+
+    @Column( nullable = false )
+    private String optionThree;
+
+    @Column( nullable = false )
+    private String optionFour;
+
+    @ManyToOne( cascade = CascadeType.ALL )
+    @JoinColumn( name = "quiz_session_id" )
+    private QuizSession quizSession;
+
+    @Override
+    public final boolean equals( Object o ) {
+        if ( this == o ) return true;
+        if ( o == null ) return false;
+        Class< ? > oEffectiveClass = o instanceof HibernateProxy ? ( ( HibernateProxy ) o ).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class< ? > thisEffectiveClass = this instanceof HibernateProxy ? ( ( HibernateProxy ) this ).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if ( thisEffectiveClass != oEffectiveClass ) return false;
+        Quiz quiz = ( Quiz ) o;
+        return getId() != null && Objects.equals( getId(), quiz.getId() );
+    }
+
+    @Override
+    public final int hashCode( ) {
+        return this instanceof HibernateProxy ? ( ( HibernateProxy ) this ).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/uiudevelopershub/thinktanku/model/quizsession/QuizSession.java
@@ -0,0 +1,36 @@
+package com.uiudevelopershub.thinktanku.model.quizsession;
+
+import com.uiudevelopershub.thinktanku.model.quiz.Quiz;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class QuizSession {
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "quiz_session_gen" )
+    @SequenceGenerator( name = "quiz_session_gen", sequenceName = "quiz_session_seq" )
+    @Column( name = "id", nullable = false )
+    private Long id;
+
+    @Column( nullable = false )
+    private String sessionId = UUID.randomUUID().toString();
+
+    @Column( nullable = false )
+    private String QuizSessionName;
+
+    @OneToMany( mappedBy = "quizSession", cascade = CascadeType.ALL, orphanRemoval = true )
+    private Set<Quiz> quizzes = new LinkedHashSet<>();
+}
