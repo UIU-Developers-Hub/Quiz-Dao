95d2f5f2e19a891b2bec09e52f3f8820e76a9957
Sajid <yeamimhossainsajid@gmail.com>
2024-10-01 16:34:09 +0600

    QuizSession ServiceImpl done

--- src/main/java/com/uiudevelopershub/thinktanku/repository/quizSessionRepo/QuizSessionRepo.java
+++ src/main/java/com/uiudevelopershub/thinktanku/repository/quizSessionRepo/QuizSessionRepo.java
@@ -1,9 +1,24 @@
 package com.uiudevelopershub.thinktanku.repository.quizSessionRepo;
 
+import com.uiudevelopershub.thinktanku.dto.response.CustomUserResponseDTO;
+import com.uiudevelopershub.thinktanku.dto.response.QuizSessionResponseDto;
 import com.uiudevelopershub.thinktanku.model.quizsession.QuizSession;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuizSessionRepo extends JpaRepository<QuizSession, Long> {
+    @EntityGraph( attributePaths = { "quiz" } )
+    @Query( """
+                SELECT
+                    q
+                FROM
+                   QuizSession q
+                WHERE
+                    q.id = :id
+            """ )
+    QuizSessionResponseDto findQuizSessionById(@Param( "id" ) Long id );
 }
--- src/main/java/com/uiudevelopershub/thinktanku/service/impl/QuizSessionServiceImpl.java
+++ src/main/java/com/uiudevelopershub/thinktanku/service/impl/QuizSessionServiceImpl.java
@@ -1,30 +1,44 @@
 package com.uiudevelopershub.thinktanku.service.impl;
 
 import com.uiudevelopershub.thinktanku.dto.request.QuizSessionRequestDto;
+import com.uiudevelopershub.thinktanku.dto.response.CustomUserResponseDTO;
 import com.uiudevelopershub.thinktanku.dto.response.QuizSessionResponseDto;
+import com.uiudevelopershub.thinktanku.model.quizsession.QuizSession;
 import com.uiudevelopershub.thinktanku.repository.quizSessionRepo.QuizSessionRepo;
 import com.uiudevelopershub.thinktanku.service.QuizSessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+@Service
 public class QuizSessionServiceImpl implements QuizSessionService {
 
-    QuizSessionRepo quizSessionRepo;
+   public  QuizSessionRepo quizSessionRepo;
 
+    @Autowired
     public QuizSessionServiceImpl(QuizSessionRepo quizSessionRepo) {
         this.quizSessionRepo = quizSessionRepo;
     }
 
+
     @Override
     public void CreateQuizSession(QuizSessionRequestDto quizSessionRequestDto) {
-
+        QuizSession quizSession = new QuizSession();
+        quizSession.setQuizSessionName(quizSessionRequestDto.QuizSessionName());
+        quizSessionRepo.save(quizSession);
     }
 
     @Override
     public QuizSessionResponseDto GetQuizSessionById(Long quizSessionId) {
-        return null;
+        QuizSessionResponseDto singleQuizSessionById = quizSessionRepo.findQuizSessionById(quizSessionId);
+        if ( Objects.isNull( singleQuizSessionById ) ) {
+            throw new RuntimeException("not found" );
+        }
+        return singleQuizSessionById;
     }
 
     @Override
     public void DeleteQuizSessionById(Long quizSessionId) {
-
+      quizSessionRepo.deleteById(quizSessionId);
     }
 }
