cb8bc41261f6d253a0ed9f220771aa948fac683e
Sajid <yeamimhossainsajid@gmail.com>
2024-10-01 10:36:18 +0600

    Added QuizRequestDto and QuizResponseDto

---
+++ src/main/java/com/uiudevelopershub/thinktanku/dto/request/QuizRequestDto.java
@@ -0,0 +1,28 @@
+package com.uiudevelopershub.thinktanku.dto.request;
+
+import com.uiudevelopershub.thinktanku.model.quizsession.QuizSession;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public record QuizRequestDto(
+
+      Long id,
+
+      String questionTitle,
+
+      String questionAnswer,
+
+      String optionOne,
+
+      String optionTwo,
+
+      String optionThree,
+
+      String optionFour,
+
+      Long quizSessionId
+
+) {
+}
---
+++ src/main/java/com/uiudevelopershub/thinktanku/dto/response/QuizResponseDto.java
@@ -0,0 +1,16 @@
+package com.uiudevelopershub.thinktanku.dto.response;
+
+public interface QuizResponseDto {
+    Long getQuizId();
+
+    String getQuestionTitle();
+
+    String getOptionOne();
+
+    String getOptionTwo();
+
+    String getOptionThree();
+
+    String getOptionFour();
+
+}
