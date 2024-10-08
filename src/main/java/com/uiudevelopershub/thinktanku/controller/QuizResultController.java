94aa0fd0339b0bbf0e51f2714a5f5f49cf460de0
Sajid <yeamimhossainsajid@gmail.com>
2024-10-08 11:27:40 +0600

    controller primerally done.lots of logical part left in service

--- src/main/java/com/uiudevelopershub/thinktanku/controller/QuizResultController.java
+++ src/main/java/com/uiudevelopershub/thinktanku/controller/QuizResultController.java
@@ -1,5 +1,30 @@
 package com.uiudevelopershub.thinktanku.controller;
 
-public class QuizResultController {
 
+import com.uiudevelopershub.thinktanku.service.impl.QuizResultServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizResultController {
+    @Autowired
+    private QuizResultServiceImpl quizResultService;
+
+    @PostMapping("/submitAnswer")
+    public ResponseEntity<?> submitAnswer(
+            @RequestParam Long userId,
+            @RequestParam Long quizSessionId,
+            @RequestParam Long questionId,
+            @RequestParam String answer) {
+        quizResultService.submitAnswer(userId, quizSessionId, questionId, answer);
+        return ResponseEntity.ok().body("Answer submitted successfully");
+    }
+
+    @GetMapping("/countCorrectAnswers/{quizSessionId}/{userId}")
+    public ResponseEntity<Long> countCorrectAnswers(@PathVariable Long quizSessionId, @PathVariable Long userId) {
+        long correctAnswersCount = quizResultService.countCorrectAnswers(quizSessionId, userId);
+        return ResponseEntity.ok().body(correctAnswersCount);
+    }
 }
